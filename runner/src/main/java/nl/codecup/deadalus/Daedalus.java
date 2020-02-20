63c4c66d1bd3abfc1fd51a896e096f99e6b1b48e
Ben <ben@yocto.nu>
2020-02-20 11:30:43 +0100

    Set up the project structure

---
+++ runner/src/main/java/nl/codecup/deadalus/Daedalus.java
@@ -0,0 +1,12 @@
+package nl.codecup.deadalus;
+
+public class Daedalus implements Runnable{
+
+	public Daedalus(String... args){
+		//TODO Implement
+	}
+
+	public void run(){
+	}
+
+}
---
+++ runner/src/main/java/nl/codecup/deadalus/Main.java
@@ -0,0 +1,9 @@
+package nl.codecup.deadalus;
+
+public class Main{
+
+	public static void main(String... args){
+		new Daedalus(args).run();
+	}
+
+}
